d7be4bedbeffde0d7144c81fa7e3b05a6166446a
d34thwings <d34thwings@gmail.com>
2023-02-16 10:07:55 +0100

    Remove seeding

--- src/repositories/prisma.ts
+++ src/repositories/prisma.ts
@@ -1,17 +1,3 @@
 import {PrismaClient} from "@prisma/client";
 
 export const prisma = new PrismaClient()
-
-prisma.user.createMany({
-  data: [{
-    email: 'john.doe@test.com',
-    name: 'John Doe',
-  }, {
-    email: 'foo.bar@test.com',
-    name: 'Foo Bar',
-  }]
-}).then(() => {
-  console.log('Seeded users')
-}, (error) => {
-  console.error(error)
-})
